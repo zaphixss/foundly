a3ba6a70e870f58d6334d673b482777a8bbfd276
zaphixss <onyekwelujoy92@gmail.com>
2026-08-08 16:19:30 +0100

    Update settings.py

--- foundly_project/settings.py
+++ foundly_project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 AUTH_USER_MODEL = 'accounts.User'
 
